bf06419c2af7b3452db93aa3ea79077421f4fcc5
Felix <yaliu@bu.edu>
2020-11-24 15:37:35 -0500

    shell

--- shell/bpe.sh
+++ shell/bpe.sh
@@ -1,12 +1,12 @@
 # Get BPE for Bora-Spanish pair
-OUTPATH=data/processed/XLM_bora/30k
+OUTPATH=data/processed/XLM_bora_es/60k
 FASTBPE=tools/fastBPE/fast
 
 # create output path
 mkdir -p $OUTPATH
 
 # learn bpe codes on the training set (or only use a subset of it)
-$FASTBPE learnbpe 30000 data/bora_es/bora_es.train > $OUTPATH/codes
+$FASTBPE learnbpe 60000 data/bora_es/bora_es.train > $OUTPATH/codes
 
 # apply BPE
 $FASTBPE applybpe $OUTPATH/train.bora_es data/bora_es/bora_es.train $OUTPATH/codes
